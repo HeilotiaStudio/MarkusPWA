0dfbccd816a25d97d871304051e73e3f79577abc
Heilotia <47873942+HeilotiaStudio@users.noreply.github.com>
2025-08-31 18:50:31 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  window.AddToHomeScreenInstance = window.AddToHomeScreen({
+    appName: 'Markuss',
+    appNameDisplay: 'standalone',
+    appIconUrl: 'apple-touch-icon.png',
+    assetUrl: 'https://cdn.jsdelivr.net/gh/philfung/add-to-homescreen@2.91/dist/assets/img/',
+    maxModalDisplayCount: 3,
+    displayOptions: { showMobile: true, showDesktop: true }
+  });
+
+  window.AddToHomeScreenInstance.show('en');
+});
+
